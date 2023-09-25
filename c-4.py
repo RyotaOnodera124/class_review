2fe4170372f8ff6eedd473ad5c141635776a5a47
RyotaOnodera124 <onoryo.0124@gmail.com>
2023-09-25 11:28:19 +0900

    c-4完了

--- c-4.py
+++ c-4.py
@@ -9,26 +9,27 @@ class Customer:
     
     def entry_fee(self):
         if self.age < 20:
-            print(1000)
+            return 1000
         elif 20 <= self.age <= 65:
-            print(1500)
+            return 1500
         elif 65 <= self.age:
-            print(1200)
+            return 1200
 
     def info_csv(self):
-        
-    
+        per_fee = self.entry_fee()
+        print(f"{self.full_name()}, {self.age}, {per_fee}")
+  
 
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
 ken.info_csv()  # "Tom Ford,57,1500" という値を返す
-print(ken.info_csv())
 
 
 tom = Customer(first_name="Tom", family_name="Ford", age= 57)
 tom.info_csv()  # "Tom Ford,57,1500" という値を返す
-print(tom.info_csv())
 
 
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
 ieyasu.info_csv()  # "Ieyasu Tokugawa,73,1200" という値を返す
-print(ieyasu.info_csv())
+
+
+# returnは関数(メソッド)から値を返すために使う、その関数が計算した結果や値を他の部分で使えるようにする
